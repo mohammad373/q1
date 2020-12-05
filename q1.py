86454fede6060090a5cb715f4ad45701fbef7215
mohammad373 <75175976+mohammad373@users.noreply.github.com>
2020-12-05 16:10:53 +0330

    Update and rename README.md to q1.py

---
+++ q1.py
@@ -0,0 +1,22 @@
+# q1
+
+import requests
+import time
+import sys
+from colorama import Fore
+
+def __1__():
+        i = input("Enter Your Address WebSite ==>  ")
+        my_list = ["admin" , "robots.txt" , "search"]
+        if "http" in i:
+          pass
+        elif "http" != i:
+          i = "http://" + i
+        for item in my_list:
+          r = i + "/" + item  
+          req = requests.get(r)
+          if req.status_code == 200:
+            print(Fore.GREEN + "This Is Ok " + Fore.GREEN + r)
+          else:
+            print(Fore.RED + "This Is Not Ok " + Fore.RED  + r)
+__1__()
