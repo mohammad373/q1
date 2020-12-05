03740db735492f520f930842a599be9fc3807d99
mohammad373 <75175976+mohammad373@users.noreply.github.com>
2020-12-05 18:55:18 +0330

    Update q1.py

--- q1.py
+++ q1.py
@@ -1,55 +1,17 @@
-# q1
-
-import requests
-import time
 import sys
+import requests
+import json
 from colorama import Fore
 
-def __1__():
-        i = input("Enter Your Address WebSite ==>  ")
-        my_list = ['robots.txt',
-      'search/',
-      'admin/',
-      'login/',
-      'sitemap.xml',
-      'sitemap2.xml',
-      'config.php',
-      'wp-login.php',
-      'log.txt',
-      'update.php',
-      'INSTALL.pgsql.txt',
-      'user/login/',
-      'INSTALL.txt',
-      'profiles/',
-      'scripts/',
-      'LICENSE.txt',
-      'CHANGELOG.txt',
-      'themes/',
-      'inculdes/',
-      'misc/',
-      'user/logout/',
-      'user/register/',
-      'cron.php',
-      'filter/tips/',
-      'comment/reply/',
-      'xmlrpc.php',
-      'modules/',
-      'install.php',
-      'MAINTAINERS.txt',
-      'user/password/',
-      'node/add/',
-      'INSTALL.sqlite.txt',
-      'UPGRADE.txt',
-      'INSTALL.mysql.txt']
-        if "http" in i:
-          pass
-        elif "http" != i:
-          i = "http://" + i
-        for item in my_list:
-          r = i + "/" + item  
-          req = requests.get(r)
-          if req.status_code == 200 or req.status_code == 405:
-            print(Fore.GREEN + "This Is Ok " + Fore.GREEN + r)
-          else:
-            print(Fore.RED + "This Is Not Ok " + Fore.RED  + r)
-__1__()
+def __8__():
+    try:
+        site = input(Fore.RED + "Enter Your Address WebSite" + Fore.YELLOW + " ==>  ")
+        r = {"removAddress" , site}
+        b = requests.post("https://domains.yougetsignal.com/domains.php" , r)
+        q = json.loads(b.content)
+        print(q)
+        for i in q["domainArray"]:
+            print(" " +Fore.GREEN+ i[0] + "\n")
+    except:
+        pass
+__8__()
